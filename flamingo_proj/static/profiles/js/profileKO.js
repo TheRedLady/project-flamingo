c53fea227655d097e900f35c779a216f16acc265
TheRedLady <christina.hristakieva@gmail.com>
2016-10-11 18:54:23 +0300

    Add share button and complete post feature

--- flamingo_proj/static/profiles/js/profileKO.js
+++ flamingo_proj/static/profiles/js/profileKO.js
@@ -1,24 +1,22 @@
 // Class to represent a row in the seat reservations grid
 //
 makePost = function(data) {
-  return new Post(data['id'], data['posted_by'], data['content'],
-                  data['created'],data['posted_by_url'], data['posted_by_name'],
-                  data['post_url']);
+  return new Post(data['id'], data['url'], data['posted_by'], data['content'],
+                  data['created'], data['shared']);
 }
 
-function Post(id, postedBy, content, created, posted_by_url, posted_by_name, post_url) {
+function Post(id, url, postedBy, content, created, shared) {
     var self = this;
-    self.post_id = id;
-    self.post_url = post_url;
+    self.id = id;
+    self.url = url;
     self.posted_by = postedBy;
-    self.posted_by_url = posted_by_url
-    self.posted_by_name = posted_by_name
     self.content = content;
     self.created = created;
+    self.shared = shared;
 
     self.removePost = function() {
       $.ajax({
-          url: "/api/posts/" + self.post_id,
+          url: "/api/posts/" + self.id,
           type: "delete"
         });
     }
@@ -28,37 +26,39 @@ function Post(id, postedBy, content, created, posted_by_url, posted_by_name, pos
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function ProfileViewModel() {
     var self = this;
-    self.firstName = ko.observable("Bert");
-    self.lastName = ko.observable("Bertington");
     self.posts = ko.observableArray([]);
     self.nextPostContent = ko.observable("");
 
-    self.fullName = ko.computed(function() {
-        return self.firstName() + " " + self.lastName();
-    }, this);
- 
     var loc = String(window.location).split("/");
     $.getJSON("/api/posts/?posted_by=" + loc[loc.length -2], function(allData) {
-        console.log(allData['results']);
         var mappedPosts = $.map(allData['results'], function(post) { return makePost(post); });
-        console.log(mappedPosts);
         self.posts(mappedPosts);
     });
 
     self.addPost = function(user_id) {
-      console.log(user_id);
-        console.log(self.nextPostContent());
         $.ajax("/api/posts/",
         {
           data: { posted_by: user_id, content: self.nextPostContent()},
           type: "post"
         }).done(function(post) {
           var new_post = makePost(post);
-          console.log(new_post);
-        self.posts.unshift(new_post);
+          self.posts.unshift(new_post);
         });
         self.nextPostContent("");
     };
+
+    self.sharePost = function(post) {
+      $.ajax({
+        url: "/api/posts/" + post.id + "/share/",
+        type: "post"
+      }).done(function(post) {
+        var new_post = makePost(post);
+        new_post.shared = true;
+        console.log(new_post);
+        self.posts.unshift(new_post);
+      });
+    }
+
     self.removePost = function(post) { 
       self.posts.remove(post);
       post.removePost();
