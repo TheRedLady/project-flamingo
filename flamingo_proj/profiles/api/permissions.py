95ea12c16f15b056576f7999b057215853539a31
TheRedLady <christina.hristakieva@gmail.com>
2016-10-25 17:56:33 +0300

    Add profile api tests

--- flamingo_proj/profiles/api/permissions.py
+++ flamingo_proj/profiles/api/permissions.py
@@ -5,7 +5,7 @@ class UserPermission(BasePermission):
 
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
-            return request.user.is_authenticated() or view.action == 'list'
+            return request.user.is_authenticated()
         else:
             if view.action == 'create':
                 return (not request.user.is_authenticated()) or request.user.is_staff
